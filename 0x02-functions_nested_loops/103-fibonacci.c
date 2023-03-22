0df2f421ff707e37db6fee8cca5c5286ac6a98a0
GKitavi <gearadkitavi@gmail.com>
2023-03-22 14:13:15 -0700

    Fibonacci sum

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -11,14 +11,14 @@ int main(void)
 	unsigned long int j, k, next, sum;
 
 	j = 1;
-	k =2;
+	k = 2;
 	sum = 0;
 
 	for (i = 1; i <= 33; ++i)
 	{
-		if(j < 4000000 && (j % 2) == 0)
+		if (j < 4000000 && (j % 2) == 0)
 		{
-			sum =sum + j;
+			sum = sum + j;
 		}
 		next = j + k;
 		j = k;
