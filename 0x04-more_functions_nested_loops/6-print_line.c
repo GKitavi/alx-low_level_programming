7cf866c489f68a077641805cc6a76f1ce938b083
GKitavi <gearadkitavi@gmail.com>
2023-03-24 03:05:43 -0700

    Prints line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,13 +2,14 @@
 
 /**
  * print_line - draws a straight line in the terminal
- * @n: number of times the character _ should be printed
+ * @n: number of times the character should be printed
  */
+
 void print_line(int n)
 {
 	if (n <= 0)
 	{
-		 _putchar('\n');
+		_putchar('\n');
 	}
 	else
 	{
