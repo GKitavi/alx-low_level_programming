21ebe3d727738da71130ded4c383a03a91216f86
GKitavi <gearadkitavi@gmail.com>
2023-03-30 01:20:13 -0700

    Concatenates 2 strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strcat - Concatenates two strings
+ * @dest: One of the two strings to be concatenated
+ * @src: second string
+ *
+ * Return: String
+ */
+char *_strcat(char *dest, char *src);
+{
+	strcat(dest, src);
+}
