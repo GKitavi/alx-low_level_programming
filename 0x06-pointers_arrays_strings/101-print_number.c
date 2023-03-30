aacac853cbdb9c0654aa373bfbb2b92205f4dfc2
GKitavi <gearadkitavi@gmail.com>
2023-03-30 13:55:01 -0700

    Prints chars

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,18 +8,18 @@
 
 void print_number(int n)
 {
-	unsigned in n1;
+	unsigned int n1;
 
 	n1 = n;
 
 	if (n < 0)
 	{
-		_putchar('-');
+		putchar('-');
 		n1 = -n;
 	}
 	if (n1 / 10 != 0)
 	{
 		print_number(n1 / 10);
 	}
-	_puchar((n1 % 10) + '0');
+	putchar((n1 % 10) + '0');
 }
