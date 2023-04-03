459f47e4ae1c297521f53c1d33de1ace179c9b01
GKitavi <gearadkitavi@gmail.com>
2023-04-03 11:27:46 -0700

    Located a substring

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,14 +8,28 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	unsigned int n = 0;
-	int i;
+	unsigned int i, j, n, check;
+	j = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		check = 0;
+
+		for (n = 0; accept[n] != '\0'; n++)
+		{
+			if (accept[n] == s[i])
+			{
+				j++;
+				check = 1;
+			}
+		}
+		
 
 	while (*s)
 	{
 		for (i = 0; accept[i]; i++)
 		{
-			if (*s == accep[i])
+			if (*s == accept[i])
 			{
 				n++;
 				break;
