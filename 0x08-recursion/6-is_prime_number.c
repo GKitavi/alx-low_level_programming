0d33a99b73d3581f8d5b8aba4b865388a123bef1
GKitavi <gearadkitavi@gmail.com>
2023-04-05 10:42:54 -0700

    Prime numbers

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -12,7 +12,7 @@ int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
-	return (actual_prime_number(n, n - 1));
+	return (actual_prime_number(n, i - 1));
 }
 
 /**
@@ -28,5 +28,5 @@ int actual_prime_number(int n, int i)
 		return (1);
 	if (n % i == 0 && i > 0)
 		return (0);
-	return (actual_prime_number(n, n - 1));
+	return (actual_prime_number(n, i - 1));
 }
