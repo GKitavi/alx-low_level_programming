a79bb3fb490b9c23266b59e335e687cf586da686
GKitavi <gearadkitavi@gmail.com>
2023-04-05 01:55:43 -0700

    Prints a string

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: input integer
+ *
+ * Return: Always 0
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*);
+		_puts_recursion(s + 1);
+	}
+
+	else
+		_putchar('\n');
+}
