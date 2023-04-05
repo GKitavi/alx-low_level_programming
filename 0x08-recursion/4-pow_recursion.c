a78e9c1c85f4f1580eb1f05b9d808e069d73479e
GKitavi <gearadkitavi@gmail.com>
2023-04-05 02:20:35 -0700

    Square root

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns value of x raised to power y
+ * @x: input value
+ * @y: input power
+ *
+ * Return: x raised to power y
+ */
+int _pow_recursion(int x, int y)
+{
+	int i = x;
+
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	i *= _pow_recursion(x, y-1);
+
+	return (i);
+}
