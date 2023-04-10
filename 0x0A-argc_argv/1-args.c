611d2fbbf1a053d322ae5c3d25698276979427ab
GKitavi <gearadkitavi@gmail.com>
2023-04-10 05:32:43 -0700

    Prints no of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <std.io>
+#define unused(x) (void)(x)
+
+/**
+ * main - Prints number of arguments
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0
+ */
+int main (int argc, char *argv[])
+{
+	unused(argv);
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
