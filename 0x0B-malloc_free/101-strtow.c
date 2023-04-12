dc085e9e942e420943deceb78c04272f3b21fcf1
GKitavi <gearadkitavi@gmail.com>
2023-04-12 13:58:35 -0700

    Updates

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -33,36 +33,32 @@ int helper(char *s)
  */
 char **strtow(char *str)
 {
-	int i, w, start, end;
-	int c = 0;
-	int k = 0;
-	int l = 0;
-	char **m, *t;
+	char **matrix, *tmp;
+	int i, k = 0, len = 0, words, c = 0, start, end;
 
-	while (*(str + l))
-		l++;
-	w = helper(str);
-	if (w == 0)
+	while (*(str + len))
+		len++;
+	words = helper(str);
+	if (words == 0)
+		return (NULL);
+	matrix = (char **) malloc(sizeof(char *) * (words + 1));
+	if (matrix == NULL)
 		return (NULL);
 
-	m == (char **) malloc(sizeof(char *) * (w + 1));
-	if (m == NULL)
-		return (NULL);
-
-	for (i = 0; i <= l; i++)
+	for (i = 0; i <= len; i++)
 	{
 		if (str[i] == ' ' || str[i] == '\0')
 		{
 			if (c)
 			{
 				end = i;
-				t = (char *) malloc(sizeof(char) * (c + 1));
-				if (t == NULL)
+				tmp = (char *) malloc(sizeof(char) * (c + 1));
+				if (tmp == NULL)
 					return (NULL);
 				while (start < end)
-					*t++ = str[start++];
-				*t = '\0';
-				m[k] = t - c;
+					*tmp++ = str[start++];
+				*tmp = '\0';
+				matrix[k] = tmp - c;
 				k++;
 				c = 0;
 			}
@@ -70,6 +66,7 @@ char **strtow(char *str)
 		else if (c++ == 0)
 			start = i;
 	}
-	m[k] = NULL;
-	return (m);
+	matrix[k] = NULL;
+
+	return (matrix);
 }
